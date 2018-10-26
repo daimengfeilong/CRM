1e81d681df88832d36669ee58a7958d961012187
周正明 <286527501@qq.com>
2018-10-26 15:02:45 +0800

    [mod]

--- src/pages/portrait/Head.js
+++ src/pages/portrait/Head.js
@@ -24,7 +24,7 @@ const Head = ({dispatch}) => {
             type:'portrait/query',
             payload:{
                 params:{
-                    className:value
+                  portraitName:value
                 }
             }
         })
