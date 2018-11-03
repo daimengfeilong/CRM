d86aa5fbbebd9fd8bb3a21030cc7e5a562937335
白秋平 <baiqiuping@hotmail.com>
2018-11-03 16:10:24 +0800

    userdetail - 状态

--- src/models/userDetail.js
+++ src/models/userDetail.js
@@ -1,4 +1,4 @@
-import { queryUserTagClassList,queryUserBasicInfo } from '../services/userList'
+import { queryUserTagClassList,queryUserBasicInfo,queryUserAccountInfo,queryUserPortraitList,queryUserInfo,updateUserRemark,queryUserLoanInfo,queryUserInsuranceInfo } from '../services/userList'
 
 export default {
   namespace: 'userDetail',
@@ -6,8 +6,14 @@ export default {
     userTagList:[],
     userNo:'',
     idCard:'',
+    userInfo:'',
     userBaseInfo:'',
-    pagination:{}
+    accountInfo:'',
+    showModel:false,
+    userPortraitList:[],
+    pagination:{},
+    description:'',
+    creditList:[]
   },
   reducers: {
     save (state, {payload}) {
@@ -19,15 +25,48 @@ export default {
   },
   effects: {
     *queryUserTagClassList ({payload}, {call, put, select}) {
-      const res = yield call(queryUserTagClassList, payload)
+      const idCard = yield select(state => state.userDetail.idCard)
+      const res = yield call(queryUserTagClassList, {idCard:idCard,type:1})
       yield put({type: "save", payload: {userTagList: res.result}})
     },
-    *saveIdCardAndUserNo({payload},{call, put, select}){
-      yield put({type: "save", payload})
-    },
     *queryUserBasicInfo ({payload}, {call, put, select}) {
-      const res = yield call(queryUserBasicInfo, payload)
+      const idCard = yield select(state => state.userDetail.idCard)
+      const res = yield call(queryUserBasicInfo, {idCard:idCard,type:1})
       yield put({type: "save", payload: {userBaseInfo: res.result}})
+    },
+    *queryUserAccountInfo ({payload}, {call, put, select}) {
+      const idCard = yield select(state => state.userDetail.idCard)
+      const res = yield call(queryUserAccountInfo,  {idCard:idCard,type:1})
+      yield put({type: "save", payload: {accountInfo: res.result}})
+    },
+    *queryUserPortraitList({payload}, {call, put, select}) {
+      const idCard = yield select(state => state.userDetail.idCard)
+      const res = yield call(queryUserPortraitList,  {idCard:idCard,type:1})
+      yield put({type: "save", payload: {userPortraitList: res.result}})
+    },
+    *queryUserInfo({payload}, {call, put, select}) {
+      const idCard = yield select(state => state.userDetail.idCard)
+      const res = yield call(queryUserInfo,  {idCard:idCard,type:1})
+      yield put({type: "save", payload: {userInfo: res.result}})
+    },
+    *updateUserRemark({payload}, {call, put, select}) {
+      const idCard = yield select(state => state.userDetail.idCard)
+      const userId = yield select(state => state.userDetail.userNo)
+      const res = yield call(updateUserRemark,  {idCard:idCard,description:payload,userId:userId})
+      // yield put({type: "save", payload: {userInfo: res.result}})
+      return res
+    },
+    *queryUserLoanInfo({payload}, {call, put, select}) {
+      const idCard = yield select(state => state.userDetail.idCard)
+      const  params={pageNo:payload.pageNo,pageSize:payload.pageSize,params:{idCard:idCard,type:1}}
+      const res = yield call(queryUserLoanInfo,  params)
+      yield put({type: "save", payload: {creditList: res.result}})
+    },
+    *queryUserInsuranceInfo({payload}, {call, put, select}) {
+      const idCard = yield select(state => state.userDetail.idCard)
+      const  params={pageNo:payload.pageNo,pageSize:payload.pageSize,params:{idCard:idCard,type:2}}
+      const res = yield call(queryUserInsuranceInfo,  params)
+      yield put({type: "save", payload: {creditList: res.result}})
     },
   }
 }
